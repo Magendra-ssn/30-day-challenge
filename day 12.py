ecda0f4aea46373b93ca5d2308f609fe5fc58eda
Magendra-ssn <85786067+Magendra-ssn@users.noreply.github.com>
2021-06-24 13:44:00 +0530

    Add files via upload

---
+++ day 12.py
@@ -0,0 +1,7 @@
+f1=open("30 days 30 hour operations.txt",'w')
+f1.write("I have completed 10 days succesfully")
+f1.close()
+
+f2=open("30 days 30 hour operations.txt",'a')
+f2.write(" \n -Magendra Kamalnath S A")
+f2.close()
